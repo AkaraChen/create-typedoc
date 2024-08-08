658c2fd15bcd17d230a056e6c45d4e7a9af9dcc6
无尘室内点墨 <akarachen@outlook.com>
2024-08-08 23:46:01 +0800

    init: init

---
+++ README.md
@@ -0,0 +1,9 @@
+# create-typedoc
+ 
+## Usage
+
+```shell
+pnpm create @akrc/typedoc
+```
+
+It will install deps and setup `vercel.json` for you.
---
+++ index.js
@@ -0,0 +1,49 @@
+import fsp from 'node:fs/promises';
+import fs from 'node:fs';
+import { confirm } from '@inquirer/prompts';
+import consola from 'consola';
+import { ensureDependencyInstalled } from 'nypm';
+import { packageUp } from 'package-up';
+import { updatePackage } from 'write-package';
+
+consola.info('Setting up dependencies...');
+await ensureDependencyInstalled('typedoc', { dev: true });
+await ensureDependencyInstalled('typedoc-material-theme', { dev: true });
+
+let entry;
+for (const tryEntry of ['index.ts', 'src/index.ts']) {
+    if (
+        fs.existsSync(tryEntry)
+    ) {
+        consola.info('Found entry file:', tryEntry);
+        entry = tryEntry;
+        break;
+    }
+}
+if (!entry) {
+    consola.error(new Error('No entry file found'));
+    process.exit(1);
+}
+
+await updatePackage(
+    // @ts-expect-error just throw
+    await packageUp(),
+    {
+        scripts: {
+            doc: `typedoc ${entry} --plugin typedoc-material-theme  --themeColor '#1C6EF3'`,
+        },
+    },
+);
+consola.success('Updated package.json');
+
+if (
+    await confirm({
+        message: 'Do you want to deploy the docs on vercel?',
+    })
+) {
+    await fsp.writeFile(
+        'vercel.json',
+        await fsp.readFile(new URL('./vercel.json', import.meta.url)),
+    );
+    consola.info('Created vercel.json');
+}
